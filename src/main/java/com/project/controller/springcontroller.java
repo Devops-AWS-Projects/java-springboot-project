cf5c64bacd75c5c9a022e00e04b716c3d4c7d4a0
Venkat-K8S <venkat.tech45@gmail.com>
2026-07-07 21:11:28 +0530

    Add EmployeeController with home and employees endpoints

---
+++ src/main/java/com/project/controller/springcontroller.java
@@ -0,0 +1,19 @@
+package com.company.employee.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Employee Management Application is Running!";
+    }
+
+    @GetMapping("/employees")
+    public String employees() {
+        return "Employee List API";
+    }
+
+}
